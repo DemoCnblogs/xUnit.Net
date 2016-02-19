5441d2f6a5fdb2d5caec5a90ee518b1a3acea856
Alan luo <iteamalan@hotmail.com>
2016-02-19 14:58:16 +0800

    add SingleBrowserFixture

--- Demo.UnitTest/Lesson03_Fixture/SharedContext_ClassFixture.cs
+++ Demo.UnitTest/Lesson03_Fixture/SharedContext_ClassFixture.cs
@@ -8,44 +8,51 @@ using Xunit.Abstractions;
 
 namespace Demo.UnitTest.Lesson03_Fixture
 {
-    public class CurrentUserFixture
+    public class SingleBrowserFixture : IDisposable
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
         public static int ExecuteCount;
-        
-        public CurrentUserFixture()
+
+        public SingleBrowserFixture()
         {
             this.UserId = 1;
             this.UserName = "North";
             ExecuteCount++;
+
+            //打开浏览器...
+        }
+
+        public void Dispose()
+        {
+            //关闭浏览器...
         }
     }
-    public class SharedContext_ClassFixture : IClassFixture<CurrentUserFixture>
+    public class SharedContext_ClassFixture : IClassFixture<SingleBrowserFixture>
     {
         ITestOutputHelper _output;
-        CurrentUserFixture _currentUser;
+        SingleBrowserFixture _fixture;
         static int _count;
-        public SharedContext_ClassFixture(ITestOutputHelper output,CurrentUserFixture user)
+        public SharedContext_ClassFixture(ITestOutputHelper output, SingleBrowserFixture fixture)
         {
             _output = output;
-            _currentUser = user;
+            _fixture = fixture;
             _count++;
         }
         #region Test case
         [Fact(DisplayName = "SharedContext.ClassFixture.Case01")]
         public void TestCase01()
         {
-            _output.WriteLine("Execute case 01! Current User:[{0}]-{1}", _currentUser.UserId, _currentUser.UserName);
-            _output.WriteLine("Execute count! Constructor:[{0}] , ClassFixture:[{1}]", _count, CurrentUserFixture.ExecuteCount);
+            _output.WriteLine("Execute case 01! Current User:[{0}]-{1}", _fixture.UserId, _fixture.UserName);
+            _output.WriteLine("Execute count! Constructor:[{0}] , ClassFixture:[{1}]", _count, SingleBrowserFixture.ExecuteCount);
 
         }
 
         [Fact(DisplayName = "SharedContext.ClassFixture.Case02")]
         public void TestCase02()
         {
-            _output.WriteLine("Execute case 01! Current User:[{0}]-{1}", _currentUser.UserId, _currentUser.UserName);
-            _output.WriteLine("Execute count! Constructor:[{0}] , ClassFixture:[{1}]", _count, CurrentUserFixture.ExecuteCount);
+            _output.WriteLine("Execute case 01! Current User:[{0}]-{1}", _fixture.UserId, _fixture.UserName);
+            _output.WriteLine("Execute count! Constructor:[{0}] , ClassFixture:[{1}]", _count, SingleBrowserFixture.ExecuteCount);
         }
         #endregion Test case
     }
